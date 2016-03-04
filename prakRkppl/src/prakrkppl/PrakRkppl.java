a97d82e75d610fbf42295c5e551ffef7a8d3bd04
rizqon <rizqon.sidik.maulana@gmail.com>
2016-03-04 16:25:14 +0700

    testing

--- prakRkppl/src/prakrkppl/PrakRkppl.java
+++ prakRkppl/src/prakrkppl/PrakRkppl.java
@@ -15,7 +15,7 @@ public class PrakRkppl {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Assalamu'alaikum");
+        System.out.println("bababa");
     }
     
 }
