4a80dcce03eec0f4bf18a57ce6be36d8c2ee0614
rizqon <rizqon.sidik.maulana@gmail.com>
2016-03-04 16:07:07 +0700

    Commit Github

---
+++ prakRkppl/src/prakrkppl/PrakRkppl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakrkppl;
+
+/**
+ *
+ * @author SB-604-21
+ */
+public class PrakRkppl {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("damang?");
+    }
+    
+}
